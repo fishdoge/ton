c41ac5a9b77828e6c98243428b933127dce8b367
mmm25002500 <marryhan25648@gmail.com>
2023-02-03 12:43:37 +0800

    Files: add gh_pages

--- tutorials/build.sh
+++
@@ -1,8 +0,0 @@
-npm init --yes
-npm install ts-node
-npm set-script watch "sass -w styles.scss docs/assets/styles.css"
-npm install ejs @types/ejs
-npm install sass
-npm install showdown @types/showdown
-npx ts-node scripts/build.ts
-npm run watch
